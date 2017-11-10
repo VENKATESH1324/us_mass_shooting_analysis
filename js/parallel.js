0c7f0834a84e9911c87756faa3799d7c99637ddb
abhishekchandran <abhishekchandran91@gmail.com>
2017-11-10 15:40:23 -0700

    parallel graph created

--- js/parallel.js
+++ js/parallel.js
@@ -3,43 +3,32 @@ class ParallelChart {
         this.margin = { top: 30, right: 10, bottom: 10, left: 10 },
         this.width = 960 - this.margin.left - this.margin.right,
         this.height = 500 - this.margin.top - this.margin.bottom;
-        //this.svg = d3.select("#parallel");        
     }
-
+    //REF: https://bl.ocks.org/mbostock/1341021
     drawChart(data) {
-        var dimensions = ["lawTotal", "totalDeaths", "shooting"]
-        var x = d3.scaleOrdinal()
-                  .domain(dimensions)
-                  .range([0, this.width]);
+        var x = d3.scale.ordinal().rangePoints([0, this.width], 1),
+            y = {};
 
-        var line = d3.line(),
-            axis = d3.axisLeft(),
+        var line = d3.svg.line(),
+            axis = d3.svg.axis().orient("left"),
             background,
             foreground;
+
         var svg = d3.select("body").append("svg")
             .attr("width", this.width + this.margin.left + this.margin.right)
             .attr("height", this.height + this.margin.top + this.margin.bottom)
             .append("g")
             .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-        
-        let temp_shooting = [];
-        let temp_totalDeaths = [];
-        let temp_lawTotal = [];
-        data.forEach((d) => {
-            temp_shooting.push(d.value.shooting);
-            temp_totalDeaths.push(d.value.totalDeaths);
-            if (d.value.lawTotal === undefined) {
-                temp_lawTotal.push(0);    
-            }
-            else 
-                temp_lawTotal.push(d.value.lawTotal);
-        });
 
-        var y_lawTotal = d3.scaleLinear().domain(d3.extent(temp_lawTotal)).range([this.height,0]),
-            y_totalDeaths = d3.scaleLinear().domain(d3.extent(temp_totalDeaths)).range([this.height, 0]),
-            y_shooting = d3.scaleLinear().domain(d3.extent(temp_shooting)).range([this.height, 0]);
-        
-        
+        var height = this.height;
+        var dimensions = [];
+        // Extract the list of dimensions and create a scale for each.
+        x.domain(dimensions = d3.keys(data[0]).filter(function (d) {
+            return d != "state" && (y[d] = d3.scale.linear()
+                .domain(d3.extent(data, function (p) { return +p[d]; }))
+                .range([height, 0]));
+        }));
+
         // Add grey background lines for context.
         background = svg.append("g")
             .attr("class", "background")
@@ -56,9 +45,44 @@ class ParallelChart {
             .enter().append("path")
             .attr("d", path);
 
+        // Add a group element for each dimension.
+        var g = svg.selectAll(".dimension")
+            .data(dimensions)
+            .enter().append("g")
+            .attr("class", "dimension")
+            .attr("transform", function (d) { return "translate(" + x(d) + ")"; });
+
+        // Add an axis and title.
+        g.append("g")
+            .attr("class", "axis")
+            .each(function (d) { d3.select(this).call(axis.scale(y[d])); })
+            .append("text")
+            .style("text-anchor", "middle")
+            .attr("y", -9)
+            .text(function (d) { return d; });
+
+        // Add and store a brush for each axis.
+        g.append("g")
+            .attr("class", "brush")
+            .each(function (d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
+            .selectAll("rect")
+            .attr("x", -8)
+            .attr("width", 16);
+
+        // Returns the path for a given data point.
         function path(d) {
-            return line(dimensions.map((p) => { return [x(p), ]; }));
+            return line(dimensions.map(function (p) { return [x(p), y[p](d[p])]; }));
         }
 
+        // Handles a brush event, toggling the display of foreground lines.
+        function brush() {
+            var actives = dimensions.filter(function (p) { return !y[p].brush.empty(); }),
+                extents = actives.map(function (p) { return y[p].brush.extent(); });
+            foreground.style("display", function (d) {
+                return actives.every(function (p, i) {
+                    return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+                }) ? null : "none";
+            });
+        }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,30 +1,5 @@
-d3.csv("data/shooting_data.csv", (error, data) => {
-	if (error) throw error;
-	var segregatedData = d3.nest()
-		.key((d) => {
-			return d.State
-		})
-		.rollup((v) => {
-			return {
-				shooting: v.length,
-				totalDeaths: d3.sum(v, (d) => {
-					return d["Total Number of Fatalities"]
-				})
-			}
-		})
-		.entries(data);
-		
-	d3.csv("data/state_law.csv",(error, lawData) => {
-		if(error) throw error;
-		segregatedData.forEach((element) => {
-			lawData.filter((d) => { 
-				if(d.state == element.key) {
-					element.value["lawTotal"] = parseInt(d.lawtotal);
-				}
-			})
-		});
-		console.log(segregatedData)
-		let parallelChart = new ParallelChart;
-		parallelChart.drawChart(segregatedData);
-	})	
-});
+d3.csv("data/cumulative_data.csv",(error, lawData) => {
+	if(error) throw error;
+	let parallelChart = new ParallelChart;
+	parallelChart.drawChart(lawData);
+})	
