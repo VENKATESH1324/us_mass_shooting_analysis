e4c7e6b1e1d84113708be89bbad757bbbef09268
abhishekchandran <abhishekchandran91@gmail.com>
2017-11-10 16:36:39 -0700

    US map created

--- js/parallel.js
+++ js/parallel.js
@@ -5,7 +5,7 @@ class ParallelChart {
         this.height = 500 - this.margin.top - this.margin.bottom;
     }
     //REF: https://bl.ocks.org/mbostock/1341021
-    drawChart(data) {
+    drawChart() {
         var x = d3.scale.ordinal().rangePoints([0, this.width], 1),
             y = {};
 
@@ -20,69 +20,71 @@ class ParallelChart {
             .append("g")
             .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
-        var height = this.height;
-        var dimensions = [];
-        // Extract the list of dimensions and create a scale for each.
-        x.domain(dimensions = d3.keys(data[0]).filter(function (d) {
-            return d != "state" && (y[d] = d3.scale.linear()
-                .domain(d3.extent(data, function (p) { return +p[d]; }))
-                .range([height, 0]));
-        }));
+        d3.csv("data/cumulative_data.csv", (error, data) => {
+            if (error) throw error;
+            var height = this.height;
+            var dimensions = [];
+            // Extract the list of dimensions and create a scale for each.
+            x.domain(dimensions = d3.keys(data[0]).filter(function (d) {
+                return d != "state" && (y[d] = d3.scale.linear()
+                    .domain(d3.extent(data, function (p) { return +p[d]; }))
+                    .range([height, 0]));
+            }));
 
-        // Add grey background lines for context.
-        background = svg.append("g")
-            .attr("class", "background")
-            .selectAll("path")
-            .data(data)
-            .enter().append("path")
-            .attr("d", path);
+            // Add grey background lines for context.
+            background = svg.append("g")
+                .attr("class", "background")
+                .selectAll("path")
+                .data(data)
+                .enter().append("path")
+                .attr("d", path);
 
-        // Add blue foreground lines for focus.
-        foreground = svg.append("g")
-            .attr("class", "foreground")
-            .selectAll("path")
-            .data(data)
-            .enter().append("path")
-            .attr("d", path);
+            // Add blue foreground lines for focus.
+            foreground = svg.append("g")
+                .attr("class", "foreground")
+                .selectAll("path")
+                .data(data)
+                .enter().append("path")
+                .attr("d", path);
 
-        // Add a group element for each dimension.
-        var g = svg.selectAll(".dimension")
-            .data(dimensions)
-            .enter().append("g")
-            .attr("class", "dimension")
-            .attr("transform", function (d) { return "translate(" + x(d) + ")"; });
+            // Add a group element for each dimension.
+            var g = svg.selectAll(".dimension")
+                .data(dimensions)
+                .enter().append("g")
+                .attr("class", "dimension")
+                .attr("transform", function (d) { return "translate(" + x(d) + ")"; });
 
-        // Add an axis and title.
-        g.append("g")
-            .attr("class", "axis")
-            .each(function (d) { d3.select(this).call(axis.scale(y[d])); })
-            .append("text")
-            .style("text-anchor", "middle")
-            .attr("y", -9)
-            .text(function (d) { return d; });
+            // Add an axis and title.
+            g.append("g")
+                .attr("class", "axis")
+                .each(function (d) { d3.select(this).call(axis.scale(y[d])); })
+                .append("text")
+                .style("text-anchor", "middle")
+                .attr("y", -9)
+                .text(function (d) { return d; });
 
-        // Add and store a brush for each axis.
-        g.append("g")
-            .attr("class", "brush")
-            .each(function (d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
-            .selectAll("rect")
-            .attr("x", -8)
-            .attr("width", 16);
+            // Add and store a brush for each axis.
+            g.append("g")
+                .attr("class", "brush")
+                .each(function (d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brush", brush)); })
+                .selectAll("rect")
+                .attr("x", -8)
+                .attr("width", 16);
+            // Returns the path for a given data point.
+            function path(d) {
+                return line(dimensions.map(function (p) { return [x(p), y[p](d[p])]; }));
+            }
 
-        // Returns the path for a given data point.
-        function path(d) {
-            return line(dimensions.map(function (p) { return [x(p), y[p](d[p])]; }));
-        }
-
-        // Handles a brush event, toggling the display of foreground lines.
-        function brush() {
-            var actives = dimensions.filter(function (p) { return !y[p].brush.empty(); }),
-                extents = actives.map(function (p) { return y[p].brush.extent(); });
-            foreground.style("display", function (d) {
-                return actives.every(function (p, i) {
-                    return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-                }) ? null : "none";
-            });
-        }
+            // Handles a brush event, toggling the display of foreground lines.
+            function brush() {
+                var actives = dimensions.filter(function (p) { return !y[p].brush.empty(); }),
+                    extents = actives.map(function (p) { return y[p].brush.extent(); });
+                foreground.style("display", function (d) {
+                    return actives.every(function (p, i) {
+                        return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+                    }) ? null : "none";
+                });
+            }
+        });
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-d3.csv("data/cumulative_data.csv",(error, lawData) => {
-	if(error) throw error;
-	let parallelChart = new ParallelChart;
-	parallelChart.drawChart(lawData);
-})	
+let map = new Map();
+map.drawChart();
+	
