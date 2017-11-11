3b9340cce84b991d722cf194c45f28efbe6fdbf3
VENKATESH1324 <venkatesh.1324@gmail.com>
2017-11-10 20:32:39 -0700

    timeline chart updated

--- js/timeline.js
+++ js/timeline.js
@@ -2,24 +2,40 @@ class TimeLine {
     constructor () {}
     drawChart() {
         var svg = d3.select("#timeline"),
-            margin = { top: 20, right: 20, bottom: 30, left: 50 },
+            margin = { top: 20, right: 0, bottom: 30, left: 70 },
             width = +svg.attr("width") - margin.left - margin.right,
-            height = +svg.attr("height") - margin.top - margin.bottom,
-            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            height = +svg.attr("height") - margin.top - margin.bottom;
+           // g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         //var parseTime = d3.timeParse("%x");
 
         // var x = d3.scaleLinear()
         //     .rangeRound([0, width]);
+ var x = d3.time.scale()
+        .range([0, width]);
 
         var y = d3.scale.linear()
-            .rangeRound([height, 0]);
+            .range([height, 0]);
 
-        var x = d3.scale.ordinal().rangeRoundBands([width, 0],0.1)//.padding(0.1);
+        var x = d3.scale.ordinal().rangeRoundBands([width, 0])//.padding(0.1);
+
+        var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("right");
+
 
         var line = d3.svg.line()
             .x(function (d) { return x(d.year); })
             .y(function (d) { return y(d.deaths); });
+
+            svg.attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
         d3.csv("data/summary_data.csv", function (d) {
             d.year = +parseInt(d.year);
@@ -30,33 +46,40 @@ class TimeLine {
             if (error) throw error;
 
             // x.domain(d3.extent(data, function(d) { return parseInt(d.year); }));
-            d3.select("#xAxis").attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
+            // d3.select("#xAxis").attr("transform",
+            //     "translate(" + margin.left + "," + margin.top + ")");
+
+           
+
             x.domain(data.map(function (d) { return d.year; }));
+
             y.domain(d3.extent(data, function (d) { return d.deaths; }));
 
-            let xAxis = g.append("g")
-                .attr("transform", "translate(0," + height + ")").attr("id", "brush_g")
-                .call(d3.svg.axis().scale(x).orient("bottom"));
+            // let xAxis = g.append("g")
+            //     .attr("transform", "translate(0," + height + ")").attr("id", "brush_g")
+            //     .call(d3.svg.axis().scale(x).orient("bottom"));
+            svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(-16," + height + ")")   //here used 18 to calibrate line chart wit x-axis ticks
+          .call(xAxis);
+              
 
+        svg.append("g")
+          .attr("class", "y axis").attr("y",-50)
+          .call(yAxis)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Death Rate").attr("x",-20).attr("y",50);
 
-            let yAxis = g.append("g")
-                .call(d3.svg.axis().scale(y).orient("left"))
-                .append("text")
-                .attr("fill", "#000")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", "0.71em")
-                .attr("text-anchor", "end")
-                .text("Deaths (Count)");
+      svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line)
+        .style({"stroke":"steelblue", "stroke-width":"1.5px", "fill":"none"});
 
-            let lineChart = g.append("path")
-                .datum(data)
-                .attr("stroke", "steelblue")
-                .attr("stroke-linejoin", "round")
-                .attr("stroke-linecap", "round")
-                .attr("stroke-width", 1.5)
-                .attr("d", line);
         });
 
         var brush = d3.svg.brush().x(x).extent([[10, 475], [width, 495]]).on("brush", brushed);
