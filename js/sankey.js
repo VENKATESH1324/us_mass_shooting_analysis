0207206e891083aa37d82486e50d76c771f43234
abhishekchandran <abhishekchandran91@gmail.com>
2017-11-28 19:03:16 -0700

    HTML page rendering done wrt nav link click

--- js/sankey.js
+++ js/sankey.js
@@ -13,7 +13,7 @@ class SankeyChart {
             color = d3.scale.category20();
 
         // append the svg canvas to the page
-        var svg = d3.select("body").append("svg")
+        var svg = d3.select("#viz_content").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
